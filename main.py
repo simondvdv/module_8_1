840e6adfdc4dc65aae74088c87ac0d86a2ea3f89
simondvdv <simondv4@gmail.com>
2024-07-31 16:31:08 +0300

    initial commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def add_everything_up(a, b):
     except TypeError as exc:
         return str(a) + str(b)
 
+
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
